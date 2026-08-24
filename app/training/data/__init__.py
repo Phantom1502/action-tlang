e6edfb5803d3662bb782ba0828da1fbb9c98c423
sullivan1502 <nguyen.huy.701@gmail.com>
2026-08-24 08:37:34 +0700

    U

--- app/training/data/__init__.py
+++ app/training/data/__init__.py
@@ -3,9 +3,7 @@ from .arguments import DataArguments
 from .data_module import (
     make_data_module
 )
-from .action_sft_dataset import ActionSFTDataset
 __all__ = [
     "DataArguments",
     "make_data_module",
-    "ActionSFTDataset"
 ]
