cfa71179c4e67512e580d93cac52c9d1f86a4222
sullivan1502 <nguyen.huy.701@gmail.com>
2026-08-26 14:43:00 +0700

    U

--- app/data_prepare/dataset_grpo_builder.py
+++ app/data_prepare/dataset_grpo_builder.py
@@ -400,7 +400,7 @@ def build_grpo_dataset(
             future_bins.append([[int(candle.open), int(candle.high), int(candle.low), int(candle.close)] for candle in future_candles])
             trends.append(best_program.think.trend.value)
             zones.append(best_program.think.zone_type.value)
-            zone_ranges.append([best_program.think.zone.lower_bin, best_program.think.zone.upper_bin] if best_program.think.zone is not None else [0, 0])
+            zone_ranges.append([int(best_program.think.zone.lower_bin), int(best_program.think.zone.upper_bin)] if best_program.think.zone is not None else [0, 0])
             actions.append(best_program.action.action_type.value)
             sls.append(int(best_program.action.sl) if best_program.action.sl is not None else 0)
             rrs.append(int(best_program.action.rr) if best_program.action.rr is not None else 0)
